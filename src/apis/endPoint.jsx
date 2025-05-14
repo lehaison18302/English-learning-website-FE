3fae18179be2adddb710e41bca79e0e50e5680b6
HaiSon <lehaison18302@gmail.com>
2025-05-14 09:29:37 +0700

    update page Home

--- src/apis/endPoint.jsx
+++ src/apis/endPoint.jsx
@@ -1,5 +1,13 @@
 const BASE_URL = "http://localhost:3000";
 
 const apiEndpoints = {
-    getUserByID: `${BASE_URL}/user`
+    getUserByID: `${BASE_URL}/user`,
+    // Lesson endpoints
+    getAllLessons: `${BASE_URL}/lessons`,
+    getLessonContent: (lessonId) => `${BASE_URL}/lessons/${lessonId}/content`,
+    submitExerciseAnswer: (lessonId, exerciseId) => `${BASE_URL}/lessons/${lessonId}/exercises/${exerciseId}/submit`,
+    getLessonProgress: (lessonId) => `${BASE_URL}/lessons/${lessonId}/progress`,
+    getVocabularyAudio: (word) => `${BASE_URL}/audio/${word}`
 }
+
+export default apiEndpoints;
